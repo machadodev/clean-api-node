99fb03805b1709639b782cceb61601a7124834f7
Paulo jorge <paulosoujava@gmail.com>
2020-10-30 15:12:57 -0300

    refactor: correction in name email validator adapter

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adatper'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
