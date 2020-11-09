8781a861d8823e57562704aafc1e798de5653bc1
Paulo jorge <paulosoujava@gmail.com>
2020-11-08 22:30:49 -0300

    feat: adapter express

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://paulao:b1eIE0s1mmZ2oVyK@cluster0.fv8pn.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://paulao:b1eIE0s1mmZ2oVyK@cluster0.fv8pn.mongodb.net/clean-node-api?retryWrites=true&w=majority',
   port: process.env.port || 5050,
   salt: process.env.salt || 12
 }
