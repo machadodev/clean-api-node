3c1dd06060336a39d7460a50b6ceff446eaa4cfc
Paulo jorge <paulosoujava@gmail.com>
2020-11-10 17:00:39 -0300

    test: in loginController

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,60 @@
+import { MissingParamError } from '../../errors'
+import { badRequset } from '../../helpers/http-helpers'
+import { EmailValidator } from '../signup/signup-protocols'
+import { LoginController } from './login'
+
+interface SutTypes {
+  sut: LoginController
+  emailValidatorStub: EmailValidator
+}
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new LoginController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+describe('Login Controller', () => {
+  test('should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequset(new MissingParamError('email')))
+  })
+  test('should return 400 if no paswword is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequset(new MissingParamError('password')))
+  })
+  test('should call eEmailValidation with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
