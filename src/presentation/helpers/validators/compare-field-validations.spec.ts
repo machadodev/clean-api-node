a48189f47d3c259bc3b4d3f0276be8c5d4e5b9d5
Paulo jorge <paulosoujava@gmail.com>
2020-11-11 16:06:12 -0300

    test: ensure CompareFieldValidation not return if validation succeds

---
+++ src/presentation/helpers/validators/compare-field-validations.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldValidation } from './compare-field-validation'
+
+const makeSut = (): CompareFieldValidation => {
+  return new CompareFieldValidation('field', 'fieldToCompare')
+}
+
+describe('Comapre Field Validation', () => {
+  test('should  return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'wrong_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('should  not return if validations succeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
