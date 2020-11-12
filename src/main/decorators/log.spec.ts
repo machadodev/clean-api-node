fdb118cb1f77c0e5ffc2dfbd542634109ac12239
Paulo jorge <paulosoujava@gmail.com>
2020-11-12 09:39:17 -0300

    refactor: data layer interface to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../data/protocols/log-error-repository copy'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository copy'
 import { AccountModel } from '../../domain/models/account'
 import { ok, serverError } from '../../presentation/helpers/http/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
