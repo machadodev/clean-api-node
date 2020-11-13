a69df7ada93e75d8e358cd9f70ebf25a5d29cdba
Paulo jorge <paulosoujava@gmail.com>
2020-11-13 11:02:56 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -3,7 +3,7 @@ import { JwtAdapter } from './jwt-adapter'
 
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
-    return new Promise(resolve => resolve('any_hash'))
+    return new Promise(resolve => resolve('any_token'))
   }
 }))
 
@@ -19,6 +19,14 @@ describe('Jwt Adapter', () => {
   test('should return a token on sign success', async () => {
     const sut = new JwtAdapter('secret')
     const accessToken = await sut.encrypt('any_id')
-    expect(accessToken).toBe('any_hash')
+    expect(accessToken).toBe('any_token')
+  })
+  test('should thorw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
