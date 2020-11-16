9a37748c50e3516b4f410c1fb226f240c84b1e77
Paulo jorge <paulosoujava@gmail.com>
2020-11-16 15:51:18 -0300

    test:  return 401

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -47,5 +47,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'paulo@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
