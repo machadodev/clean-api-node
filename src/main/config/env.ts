57143ccf38d791ee786134ae16e7f26755839fea
Paulo jorge <paulosoujava@gmail.com>
2020-11-16 15:43:26 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://paulao:b1eIE0s1mmZ2oVyK@cluster0.fv8pn.mongodb.net/clean-node-api?retryWrites=true&w=majority',
   port: process.env.port || 5050,
-  salt: process.env.salt || 12
+  salt: process.env.salt || 12,
+  jwtSecret: process.env.JWT_SECRET || 'P*a*u*10'
 }
