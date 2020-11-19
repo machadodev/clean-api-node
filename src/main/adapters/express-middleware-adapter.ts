754ae1610b9c9fb0be98850210be94f1ce5c0a02
Paulo jorge <paulosoujava@gmail.com>
2020-11-19 19:20:03 -0300

    test: return 403 on add survey wuthout accessToken

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,20 @@
+import { Controller, HttpRequest } from '../../presentation/protocols'
+import { NextFunction, Request, Response } from 'express'
+
+export const adaptMiddleware = (controller: Controller) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      // tudo que tem no body coloca no req ( padrão de projeto PROXY)
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
--- src/main/routes/sourvey-routes.test.ts
+++ src/main/routes/sourvey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('should return 204 on add survey success ', async () => {
+    test('should return 40 on add survey success ', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -32,7 +32,7 @@ describe('Survey Routes', () => {
             answer: 'Answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
