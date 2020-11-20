bdce9a871bcab917d85e963e789339228a0277ea
Paulo jorge <paulosoujava@gmail.com>
2020-11-20 14:57:31 -0300

    test: return 200 on load survey with a valid accessToken

--- src/main/routes/sourvey-routes.test.ts
+++ src/main/routes/sourvey-routes.test.ts
@@ -73,5 +73,34 @@ describe('Survey Routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+    test('should return 200 on load survey with a valid accessToken ', async () => {
+      const res = await accountCollection.insertOne({
+        nsmr: 'PAULO',
+        email: 'paulo@gmail.com',
+        password: '123'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
