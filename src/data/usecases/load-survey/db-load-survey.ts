519272addc1bcc005deded6bc05bf9f7514f8bcc
Paulo jorge <paulosoujava@gmail.com>
2020-11-20 13:22:25 -0300

    feat: ensure DbLoadServeys call loadServyRepositoru

---
+++ src/data/usecases/load-survey/db-load-survey.ts
@@ -0,0 +1,12 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { LoadSurveyRepository } from '../addSurvey/add-survey-protocols'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly loadSurveysRepository: LoadSurveyRepository) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return []
+  }
+}
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,3 +1,4 @@
+
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
