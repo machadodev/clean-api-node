7b6beba2a1da36e6b0ec818a5f3441440fc159ed
PAULO OLIVEIRA <paulosoujava@gmail.com>
2020-11-23 09:20:04 -0300

    Delete env.ts
    delete env

--- src/main/config/env.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://paulao:b1eIE0s1mmZ2oVyK@cluster0.fv8pn.mongodb.net/clean-node-api?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'P@a1l0o'
-}
